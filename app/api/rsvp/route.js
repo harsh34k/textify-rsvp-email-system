ef8bef74f95138d6edc81c1e7063ea7aa26dc6f2
Harsh Kumar <harsh34k@gmail.com>
2025-04-30 00:21:59 +0530

    Refactor code structure for improved readability and maintainability

---
+++ app/api/rsvp/route.js
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server';
+
+export async function POST(request) {
+    const { email, response, sheetId } = await request.json();
+
+    const url = process.env.NEXT_PUBLIC_GSCRIPT_URL;
+
+    // const body = {
+    //     email,
+    //     response,
+    //     sheetId,
+    // };
+    console.log("response", response);
+
+
+    const res = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            action: "updateResponse",
+            sheetId,
+            email,
+            response
+        })
+    });
+
+    const data = await res.json();
+
+    if (data.status === 'success') {
+        return NextResponse.json({ message: 'RSVP submitted successfully' });
+    } else {
+        return NextResponse.json({ error: 'Error occurred' });
+    }
+}
